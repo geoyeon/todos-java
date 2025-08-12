4c28d3bfec28e0bc7c14ed43de80d776fc1f0ec9
geoyeon <geoyeon@gmail.com>
2025-08-12 23:55:00 +0900

    test : 테스트 코드 테스트로 일부 서비스만 추가

--- src/main/java/com/geoyeon/java/todo/dto/TodoCreateRequest.java
+++ src/main/java/com/geoyeon/java/todo/dto/TodoCreateRequest.java
@@ -1,19 +1,21 @@
 package com.geoyeon.java.todo.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@ToString
 public class TodoCreateRequest {
     @NotBlank(message = "제목은 필수입니다.")
-//    @Min(value = 3, message = "제목은 3자이상 필수입니다.")
     private String title;
 
     private String memo;
